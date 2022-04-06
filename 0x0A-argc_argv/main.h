a4353adde53fa5c569c72ebb8610e15062dd0377
Ramuel27 <kirstos27@gmail.com>
2022-04-06 05:51:49 -0700

    Header file for argc and argv project

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,10 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void _putchar(char);
-int _atoi(char *s);
-char _isPosorNeg(char *s);
-int _isdigit(char c);
-int _strNumsOnly(char *s);
-
-#endif /* MAIN_H */
+int _putchar(char c);
